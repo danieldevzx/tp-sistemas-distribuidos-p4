c33e909fa629f7646953c58e619afd1f2d50f0af
ViniciusOliveira2004 <vinicius.oliveira3@ufv.br>
2026-05-20 22:36:34 -0300

    feat: usuário controller

--- server/controller/usuario_controller.py
+++ server/controller/usuario_controller.py
@@ -1,11 +1,16 @@
 from modelo.usuario_modelo import UsuarioModelo
 import random
+from modelo.jogo import Jogo
 
 class UsuarioController:
     """
     Controlador responsável por processar as requisições de negócio 
     relacionadas a usuários.
     """
+    def __init__(self):
+        self.jogo = Jogo()
+
+
     @staticmethod
     def cadastrar(dados_payload):
         usuario = dados_payload.get('username')
@@ -25,8 +30,19 @@ class UsuarioController:
             return False, "Usuário e senha são obrigatórios"
             
         resultado = UsuarioModelo.buscar_por_credenciais(usuario, senha)
-        
+
         if resultado:
-            return True, "Login realizado com sucesso"
+            return True, { "mensagem": "Login realizado com sucesso", "usuario": resultado }
         else:
             return False, "Usuário ou senha inválidos"
+    
+    @staticmethod
+    def interagir_campo(dados_payload):
+        usuario = dados_payload.get('usuario')
+        posicao = dados_payload.get('posicao')
+        if usuario[3] == 1:
+            
+        else:
+
+
+        return True, "CAPETA"
